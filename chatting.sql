61f613ec9fd69340814ae0c0d3e0ea2b13cdff12
Arjikusna M <arjiduba@gmail.com>
2025-05-26 20:06:00 +0700

    tambah database

--- chatting.sql
+++ chatting.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Bulan Mei 2025 pada 18.28
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Generation Time: May 26, 2025 at 02:59 PM
+-- Server version: 10.4.25-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,57 +24,83 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `chat`
+-- Table structure for table `chat`
 --
 
 CREATE TABLE `chat` (
   `id_chat` int(11) NOT NULL,
   `id_sender` int(11) NOT NULL,
   `id_receiver` int(11) NOT NULL,
-  `Message` text DEFAULT NULL,
-  `Timestamp` datetime DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `message` text NOT NULL,
+  `timestamp` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chat`
+--
+
+INSERT INTO `chat` (`id_chat`, `id_sender`, `id_receiver`, `message`, `timestamp`) VALUES
+(15, 10, 6, 'jir', '2025-05-26 06:06:51'),
+(20, 12, 6, 'halo', '2025-05-26 06:31:57'),
+(21, 12, 6, 'lol banget luwh', '2025-05-26 06:36:55'),
+(22, 6, 12, 'hehe', '2025-05-26 06:37:19'),
+(23, 6, 12, 'ngga bisa gweh\njir', '2025-05-26 06:38:28');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `contact`
+-- Table structure for table `contact`
 --
 
 CREATE TABLE `contact` (
   `id_contact` int(11) NOT NULL,
   `id_useradder` int(11) NOT NULL,
   `id_userreceiver` int(11) NOT NULL,
-  `Nickname` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `nickname` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `contact`
+--
+
+INSERT INTO `contact` (`id_contact`, `id_useradder`, `id_userreceiver`, `nickname`) VALUES
+(15, 10, 6, 'ggg nickname'),
+(16, 6, 10, 'xxx'),
+(23, 12, 6, 'ggg nickname'),
+(24, 6, 12, 'aaa'),
+(27, 11, 12, 'aaa'),
+(28, 12, 11, 'yyy');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
-  `Username` varchar(50) NOT NULL,
-  `Password` varchar(255) NOT NULL,
-  `Nickname` varchar(50) DEFAULT NULL,
-  `Email` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `user`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id_user`, `Username`, `Password`, `Nickname`, `Email`) VALUES
-(1, 'danielmew', '123456', 'Daniel Mewing', 'ber@gmail.com');
+INSERT INTO `user` (`id_user`, `username`, `password`, `nickname`, `email`) VALUES
+(6, 'ggg', '$2b$10$Aoj2FYqzHLlUJbtPbJHnx.c9cNs3y1VskRDk6aTVoDxOKhdFq.r5q', 'ggg', 'ggg@gmail.com'),
+(10, 'xxxbaru', '$2b$10$VEfQa4KPckNTsAeKwg9glOAdLjUr3YFfQi/ZG88vS6Um6e1zLg.2S', 'xxx', 'xxx@gmail.com'),
+(11, 'yyy', '$2b$10$7s.K8vw1UJy8tMns1ILZz./QdGB0J7g2ELJZgJGUwrGiKC4ir.ELG', 'yyy', 'yyy@gmail.com'),
+(12, 'aaa', '$2b$10$sxryZ5jM3kWiQ87MWEbd7OGqQSv6y7nxOhWE9mtUTRc.ArIUj0BEm', 'aaa', 'aaa@gmail.com');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `chat`
+-- Indexes for table `chat`
 --
 ALTER TABLE `chat`
   ADD PRIMARY KEY (`id_chat`),
@@ -82,7 +108,7 @@ ALTER TABLE `chat`
   ADD KEY `id_receiver` (`id_receiver`);
 
 --
--- Indeks untuk tabel `contact`
+-- Indexes for table `contact`
 --
 ALTER TABLE `contact`
   ADD PRIMARY KEY (`id_contact`),
@@ -90,52 +116,52 @@ ALTER TABLE `contact`
   ADD KEY `id_userreceiver` (`id_userreceiver`);
 
 --
--- Indeks untuk tabel `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`),
-  ADD UNIQUE KEY `Username` (`Username`),
-  ADD UNIQUE KEY `Email` (`Email`);
+  ADD UNIQUE KEY `username` (`username`),
+  ADD UNIQUE KEY `email` (`email`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `chat`
+-- AUTO_INCREMENT for table `chat`
 --
 ALTER TABLE `chat`
-  MODIFY `id_chat` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_chat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
--- AUTO_INCREMENT untuk tabel `contact`
+-- AUTO_INCREMENT for table `contact`
 --
 ALTER TABLE `contact`
-  MODIFY `id_contact` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_contact` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
--- AUTO_INCREMENT untuk tabel `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `chat`
+-- Constraints for table `chat`
 --
 ALTER TABLE `chat`
-  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `user` (`id_user`),
-  ADD CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`id_receiver`) REFERENCES `user` (`id_user`);
+  ADD CONSTRAINT `chat_ibfk_1` FOREIGN KEY (`id_sender`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `chat_ibfk_2` FOREIGN KEY (`id_receiver`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
--- Ketidakleluasaan untuk tabel `contact`
+-- Constraints for table `contact`
 --
 ALTER TABLE `contact`
-  ADD CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`id_useradder`) REFERENCES `user` (`id_user`),
-  ADD CONSTRAINT `contact_ibfk_2` FOREIGN KEY (`id_userreceiver`) REFERENCES `user` (`id_user`);
+  ADD CONSTRAINT `contact_ibfk_1` FOREIGN KEY (`id_useradder`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `contact_ibfk_2` FOREIGN KEY (`id_userreceiver`) REFERENCES `user` (`id_user`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
